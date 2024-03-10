88755e06860e6555ffed729bdcb8ece916838638
jensonjenkins <dave.martheen@gmail.com>
2024-03-11 00:20:52 +0800

    change name to availability

--- src/main/java/com/idp/server/product/Product.java
+++ src/main/java/com/idp/server/product/Product.java
@@ -17,7 +17,7 @@ public class Product {
     private LocalDate createdAt;
     private String categoryId;
     private String imageLink;
-    private Integer quantity;
+    private Integer availability;
 
     public Product() {
     }
@@ -27,14 +27,14 @@ public class Product {
                    Double price,
                    LocalDate createdAt,
                    String categoryId, String imageLink,
-                   Integer quantity) {
+                   Integer availability) {
         this.name = name;
         this.description = description;
         this.price = price;
         this.createdAt = createdAt;
         this.categoryId = categoryId;
         this.imageLink = imageLink;
-        this.quantity = quantity;
+        this.availability = availability;
     }
 
     public Long getId() {
@@ -69,12 +69,12 @@ public class Product {
         return categoryId;
     }
 
-    public Integer getQuantity() {
-        return quantity;
+    public Integer getAvailability() {
+        return availability;
     }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
+    public void setAvailability(Integer availability) {
+        this.availability = availability;
     }
 
     public void setCategoryId(String categoryId) {
